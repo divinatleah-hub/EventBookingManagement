0fc276d2f30ec6ea99cb94d8707eea815c7ba012
divinatleah-hub <divinatleah@gmail.com>
2026-09-22 19:32:33 +0530

    Add user admin and organizer repositories

---
+++ backend/src/main/java/event_ticketing/repository/OrganizerRepository.java
@@ -0,0 +1,10 @@
+package event_ticketing.repository;
+
+import event_ticketing.entity.Organizer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface OrganizerRepository extends JpaRepository<Organizer, Integer> {
+
+    Optional<Organizer> findByEmail(String email);
+}
